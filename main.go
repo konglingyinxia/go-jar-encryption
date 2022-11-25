b44b3691735bb3616955a0ab4eea9cfb0911edb4
konglingyinxia <hedankly@foxmail.com>
2022-11-25 17:43:12 +0800

    添加动态随即

--- main.go
+++ main.go
@@ -3,19 +3,36 @@ package main
 import (
 	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/app"
+	"fyne.io/fyne/v2/container"
+	"fyne.io/fyne/v2/widget"
 	"github.com/konglingyinxia/go-jar-encryption/logger"
 	"github.com/konglingyinxia/go-jar-encryption/resource"
 )
 
+var win fyne.Window
+
 func main() {
 	a := app.NewWithID("com.mzydz.jarencryption")
-	win := a.NewWindow("jar包加密")
+	win = a.NewWindow("jar包加密")
 	//  //定义加密密码
 	//定义文件选择框  //定义文件转码输出框
+	layoutCreate()
 
 	win.SetIcon(resource.LoadResource(resource.IconPath))
+	win.SetMaster()
 	win.Resize(fyne.NewSize(600, 450))
 	logger.Log().Info("启动完成.............")
 	win.ShowAndRun()
 	logger.Log().Info("服务退出.............")
+}
+
+// 创建布局
+func layoutCreate() {
+
+	//自定义密码
+	input := widget.NewEntry()
+	input.SetText("20085151")
+	line := container.NewVBox(widget.NewLabel("密码:"), input)
+	win.SetContent(line)
+
 }
--- resource/resource_load.go
+++ resource/resource_load.go
@@ -3,9 +3,37 @@ package resource
 import (
 	"fmt"
 	"fyne.io/fyne/v2"
+	"github.com/konglingyinxia/go-jar-encryption/logger"
+	"io/fs"
+	"math/rand"
+	"os"
+	"path/filepath"
+	"time"
 )
 
 const IconPath = "resource/img/favicon.ico"
+const fontPath = "resource/font"
+
+var fontFiles []string
+
+func init() {
+	err := filepath.Walk(fontPath, func(path string, info fs.FileInfo, err error) error {
+		if !info.IsDir() {
+			fontFiles = append(fontFiles, path)
+		}
+		return err
+	})
+	if err != nil {
+		logger.Log().Error("初始化字体库失败", err)
+	}
+	rand.Seed(time.Now().UnixNano())
+	i := rand.Intn(len(fontFiles))
+	path := fontFiles[i]
+	err = os.Setenv("FYNE_FONT", path)
+	if err != nil {
+		logger.Log().Error("初始化字体库环境变量设置失败", err)
+	}
+}
 
 func LoadResource(path string) fyne.Resource {
 	r, err := fyne.LoadResourceFromPath(path)
